7d2f7649ee97c2b579ceb40d2afa2622a84cb9c3
vgingole <vgingole@RFNCRDPVM04>
2025-03-06 08:28:20 +0530

    Add project files.

---
+++ LabraryManagement/Controllers/borrowController.cs
@@ -0,0 +1,29 @@
+﻿using LabraryManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LabraryManagement.Controllers
+{
+    public class borrowController : Controller
+    {
+        private static List<borrowModel> _bookList = new List<borrowModel>();
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(_bookList);
+        }
+
+        [HttpPost]
+        public IActionResult Index(borrowModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                _bookList.Add(model); // Add the book to the list
+
+                // Redirect to the Index page after saving
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+    }
+}
