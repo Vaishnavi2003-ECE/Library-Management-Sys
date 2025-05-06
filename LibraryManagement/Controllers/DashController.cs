f2c3e2fbdaea39ed5cf1e5aeac981c2f3c9a7193
vaishnavi ingole <vaishnaviingole340@gmail.com>
2025-05-06 12:39:06 +0530

    update libraryManagement

---
+++ LibraryManagement/Controllers/DashController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LabraryManagement.Controllers
+{
+    public class DashController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+       
+    }
+}
---
+++ LibraryManagement/Controllers/returnController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LabraryManagement.Controllers
+{
+    public class returnController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
